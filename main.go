b69745727db37a8d30eee72255385229cc2ab68e
Rama-85 <113574143+Rama-85@users.noreply.github.com>
2023-04-21 15:50:43 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -4,55 +4,17 @@ import (
 	"fmt"
 )
 
-type Author struct {
-	Name string
-	age  int
-	sal  int
-	city string
-}
-type Book struct {
-	Name  string
-	pages int
-	cost  int
-}
-
 func main() {
-	a1 := new(Author)
-
-	a1.Name = "Chand"
-	a1.age = 50
-	a1.sal = 400000
-	a1.city = "Hyd"
-
-	fmt.Println(a1)
-
-	a2 := Author{"Krishna", 30, 300000, "Vizag"}
-	fmt.Println(a2)
-
-	var a3 = Author{
-		Name: "Vijay",
-		age:  40,
-		sal:  100000,
-		city: "Mumbai",
-	}
-	fmt.Println(a3)
-
-	b1 := new(Book)
-
-	b1.Name = "Magic book"
-	b1.pages = 50
-	b1.cost = 400
-
-	fmt.Println(b1)
-
-	b2 := Book{"Golang", 100, 500}
-	fmt.Println(b2)
-
-	var b3 = Book{
-		Name:  "Computers",
-		pages: 200,
-		cost:  1000,
-	}
-	fmt.Println(b3)
+	var x = 5
+	var y = 6
+	a := &x
+	b := &y
+	fmt.Println("before swap:", x, y)
+	x = *a
+	y = *b
+	temp := *a
+	*a = *b
+	*b = temp
+	fmt.Println("After swap,x and y values are:", x, y)
 
 }
